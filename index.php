33df958a9c9389f4a7fe4b5676d9a3b07e7fb4a3
alessandrostorchi92 <storchi.alle@gmail.com>
2023-09-19 22:54:00 +0200

    Creazione classe Movie

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Creare un file index.php in cui importare una classe Movie, istanziati almeno due oggetti Movie e stampati a schermo i valori delle relative proprietà">
+    <title>PHP OOP</title>
+
+    <!-- Framework Bootstrap css  -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    <!-- Font Awesome CDN  -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- Custom css  -->
+
+    <link rel="stylesheet" href="./css/style.css">
+
+</head>
+
+
+<body>
+
+
+</body>
+
+
+</html>
